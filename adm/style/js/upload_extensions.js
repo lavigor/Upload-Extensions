eefb1afe92683811338d64d1f7f384aa8b46d578
lavigor <lavigor@users.noreply.github.com>
2014-08-10 17:35:47 +0400

    Indentation fix.

--- adm/style/js/upload_extensions.js
+++ adm/style/js/upload_extensions.js
@@ -1,6 +1,6 @@
 ; (function ($, window, document) {
 	// do stuff here and use $, window and document safely
-	// https://www.phpbb.com/community/viewtopic.php?p=13589106#p13589106	
+	// https://www.phpbb.com/community/viewtopic.php?p=13589106#p13589106
 	$("a.simpledialog").simpleDialog({
 	    opacity: 0.1,
 	    width: '650px',
